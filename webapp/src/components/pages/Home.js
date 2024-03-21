555ade4ea02bfcf9518acd333d12f59ccb17be6a
Zohaib Akhtar Kausar <zohaib0614@gmail.com>
2024-03-21 04:31:33 +0100

    AddUser + Vista Home

---
+++ webapp/src/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../App.css';
+import { Button } from '../Button';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/home.mp4' autoPlay loop muted data-testid="home-video"/>
+      <h1>WIQ</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+          to='/Jugar'
+        >
+          JUGAR
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
