0b86f1423de20b7c02688fc242503f2fccddad36
yagonavajas <uo287746@uniovi.es>
2024-03-31 13:06:02 +0200

    test historial funcionan

--- users/historyservice/history-service.test.js
+++ users/historyservice/history-service.test.js
@@ -1,46 +1,42 @@
 const request = require('supertest');
-const mongoose = require('mongoose');
-const app = require('./history-service');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const History = require('./history-model');
 
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongodb = new MongoMemoryServer();
+let mongodb;
+let app;
 
 beforeAll(async () => {
+  mongodb = await MongoMemoryServer.create();
   const uri = await mongodb.getUri();
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  process.env.MONGODB_URI = uri;
+  app = require('./history-service');
 });
 
 afterAll(async () => {
-  await mongoose.disconnect();
+  app.close();
   await mongodb.stop();
 });
 
-beforeEach(async () => {
-  await History.deleteMany({});
-});
-
 describe('POST /savehistory', () => {
-  it('should save history entry for a new user that plays a game', async () => {
+  test('should save history entry for a new user that plays a game', async () => {
     const userData = {
       username: 'testuser',
       NumPreguntasJugadas: 5,
       NumAcertadas: 3
     };
 
-    const response = await request(app)
-      .post('/savehistory')
-      .send(userData)
-      .expect(200);
+    const response = await request(app).post('/savehistory').send(userData);
 
+    expect(response.status).toBe(200);
     expect(response.body.username).toBe(userData.username);
     expect(response.body.NumJugadas).toBe(1);
     expect(response.body.NumPreguntasJugadas).toBe(userData.NumPreguntasJugadas);
     expect(response.body.NumAcertadas).toBe(userData.NumAcertadas);
     expect(response.body.NumFalladas).toBe(userData.NumPreguntasJugadas - userData.NumAcertadas);
+
   });
 
-  it('should update history entry for an existing user', async () => {
+  test('should update history entry for an existing user', async () => {
     // Crear una entrada de historial existente en la base de datos
     await History.create({
       username: 'existinguser',
@@ -56,11 +52,9 @@ describe('POST /savehistory', () => {
       NumAcertadas: 2
     };
 
-    const response = await request(app)
-      .post('/savehistory')
-      .send(userData)
-      .expect(200);
+    const response = await request(app).post('/savehistory').send(userData)
 
+    expect(response.status).toBe(200);
     expect(response.body.username).toBe(userData.username);
     expect(response.body.NumJugadas).toBe(2); // Verificar que se ha incrementado el número de jugadas
     expect(response.body.NumPreguntasJugadas).toBe(10); // Verificar que se han añadido las nuevas preguntas jugadas
@@ -70,34 +64,31 @@ describe('POST /savehistory', () => {
 });
 
 describe('GET /gethistory', () => {
-  it('should get history entry for an existing user', async () => {
+  test('should get history entry for an existing user', async () => {
     // Crear una entrada de historial existente en la base de datos
     await History.create({
-      username: 'existinguser',
+      username: 'existinguser2',
       NumJugadas: 1,
       NumPreguntasJugadas: 5,
       NumAcertadas: 3,
       NumFalladas: 2
     });
 
-    const response = await request(app)
-      .get('/gethistory')
-      .query({ username: 'existinguser' })
-      .expect(200);
-
-    expect(response.body.username).toBe('existinguser');
+    const response = await request(app).get('/gethistory').query({ username: 'existinguser2' })
+    
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('username', 'NumJugadas','NumPreguntasJugadas','NumAcertadas','NumFalladas');
+    expect(response.body.username).toBe('existinguser2');
     expect(response.body.NumJugadas).toBe(1);
     expect(response.body.NumPreguntasJugadas).toBe(5);
     expect(response.body.NumAcertadas).toBe(3);
     expect(response.body.NumFalladas).toBe(2);
   });
 
-  it('should create new history entry for a non-existing user', async () => {
-    const response = await request(app)
-      .get('/gethistory')
-      .query({ username: 'nonexistinguser' })
-      .expect(200);
+  test('should create new history entry for a non-existing user', async () => {
+    const response = await request(app).get('/gethistory').query({ username: 'nonexistinguser' });
 
+    expect(response.status).toBe(200);
     expect(response.body.username).toBe('nonexistinguser');
     expect(response.body.NumJugadas).toBe(0);
     expect(response.body.NumPreguntasJugadas).toBe(0);
