7584ff62e16bc94a222d616523141f66e87fd55f
yagonavajas <uo287746@uniovi.es>
2024-04-23 20:05:54 +0200

    cambios menores

--- webapp/src/components/pages/Ayuda.js
+++ webapp/src/components/pages/Ayuda.js
@@ -13,9 +13,9 @@ function Ayuda() {
                     <p>Para registrarte, haz clic en el botón "Registrarse" en la página principal. Completa el formulario con la información requerida (nombre de usuario y contraseña). ¡Listo! Ahora puedes iniciar sesión y empezar a jugar.</p>
 
                     <h3>¿Cómo inicio sesión?</h3>
-                    <p>Ingresa a la página principal y haz clic en "Iniciar Sesión". Escribe tu nombre de usuario y contraseña. Si tus credenciales son correctas, serás redirigido a la pagina principal para poder jugar o realizar otras funciones.</p>
+                    <p>Ingresa a la página principal y haz clic en "Entrar". Escribe tu nombre de usuario y contraseña. Si tus credenciales son correctas, serás redirigido a la pagina principal para poder jugar o realizar otras funciones.</p>
                 </section>
-
+                <br />
                 <section>
                     <h2>2. Cómo Jugar</h2>
                     <h3>Inicio del Juego</h3>
@@ -27,7 +27,7 @@ function Ayuda() {
                     <h3>Final del Juego</h3>
                     <p>Al terminar de responder las 5 preguntas, verás tu puntuación en la pantalla. Podrás revisar las respuestas correctas e incorrectas.</p>
                 </section>
-
+                <br />
                 <section>
                     <h2>3. Historial y Ranking</h2>
                     <h3>¿Cómo puedo ver mi historial de juego?</h3>
@@ -36,7 +36,7 @@ function Ayuda() {
                     <h3>¿Cómo funciona el ranking?</h3>
                     <p>El ranking se basa en una metrica Bayesiana que calcula los 10 mejores jugadores a partir de sus resultados. Para ver el ranking, ve a la sección "Ranking" en la barra de navegación. Compararás tus puntuaciones con las de otros jugadores y ver tu posición en la tabla de líderes.</p>
                 </section>
-
+                <br />
                 <section>
                     <h2>4. API REST de la aplicación</h2>
                     <p>La API REST proporciona acceso programático a funciones clave del juego, permitiendo a los desarrolladores interactuar con la base de datos de jugadores y el sistema de preguntas de manera eficiente. A continuación, se detallan los endpoints principales y cómo utilizarlos.</p>
@@ -95,7 +95,7 @@ function Ayuda() {
                     <h3>Autenticación y Seguridad</h3>
                     <p>Todos los endpoints requieren una autenticación para asegurar que solo usuarios autorizados puedan acceder a la información sensible. La autenticación se realiza mediante tokens JWT que deben ser incluidos en las cabeceras de cada solicitud.</p>
                 </section>
-
+                <br />
                 <section>
                     <h2>5. Otras Funcionalidades</h2>
                     <ul>
@@ -103,7 +103,7 @@ function Ayuda() {
                         <li>¿El juego tiene algún costo? No, el juego es completamente gratuito. Solo necesitas registrarte para jugar.</li>
                     </ul>
                 </section>
-
+                <br />
                 <section>
                     <h2>6. Contacto</h2>
                     <p>¿Necesitas más ayuda? Si tienes preguntas adicionales o necesitas asistencia, no dudes en contactarnos. Puedes enviarnos un correo electrónico a :</p>
@@ -112,10 +112,10 @@ function Ayuda() {
                             <a href="mailto:uo287746@uniovi.es">uo287746@uniovi.es</a>, 
                         </li>
                         <li>
-                            <a href="mailto:uo287746@uniovi.es">uo287746@uniovi.es</a>, 
+                            <a href="mailto:uo291060@uniovi.es">uo291060@uniovi.es</a>, 
                         </li>
                         <li>
-                            <a href="mailto:uo287746@uniovi.es">uo287746@uniovi.es</a>, 
+                            <a href="mailto:uo277269@uniovi.es">uo277269@uniovi.es</a>, 
                         </li>
                     </ul>        
                 </section>
