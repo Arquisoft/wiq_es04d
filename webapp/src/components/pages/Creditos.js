3cc583a9dab76fc700d85f6d439be9b1515451bc
yagonavajas <uo287746@uniovi.es>
2024-04-25 21:12:18 +0200

    añadida musica al video

--- webapp/src/components/pages/Creditos.js
+++ webapp/src/components/pages/Creditos.js
@@ -2,17 +2,19 @@ import React, { useRef, useEffect } from 'react';
 import '../../App.css';
 
 function Creditos() {
-    const videoRef = useRef(null); // Crear la referencia
+    // const videoRef = useRef(null); // Crear la referencia
 
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.playbackRate = 0.7; // Establecer la velocidad de reproducción al 80%
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (videoRef.current) {
+    //         videoRef.current.playbackRate = 0.7; // Establecer la velocidad de reproducción al 80%
+    //     }
+    // }, []);
+
+    // ref={videoRef}
 
     return (
         <div className='hero-container'>
-            <video ref={videoRef} src='/videos/creditos.mp4' autoPlay loop muted playsInline data-testid="creditos-video"/>
+            <video src='/videos/creditos.mp4' autoPlay playsInline data-testid="creditos-video"/>
         </div>
     );
 }
